21267eace81017cc07a3b59c86891de1c579f47c
Simiso K Mdluli <simisok22@gmail.com>
2025-03-06 13:07:08 +0200

    New Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -10,42 +10,48 @@ function Header() {
         </Link>
       </div>
       <div className="flex-none">
-        <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+        <div className="lg:hidden">
+          {" "}
+          {/* Mobile menu */}
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li className="bg-[#c79a96] text-white hover:bg-[#a87672]">
-              <Link to="/">About</Link>
-            </li>
-            <li className="bg-[#c79a96] text-white hover:bg-[#a87672]">
-              <Link to="/beauty-therapy">Beauty Therapy</Link>
-            </li>
-            <li className="bg-[#c79a96] text-white hover:bg-[#a87672]">
-              <Link to="/blog">My Blog</Link>
-            </li>
-            <li className="bg-[#c79a96] text-white hover:bg-[#a87672]">
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
+              <li className="bg-[#c79a96] text-white hover:bg-[#a87672]">
+                <Link to="/">About</Link>
+              </li>
+              <li className="bg-[#c79a96] text-white hover:bg-[#a87672]">
+                <Link to="/beauty-therapy">Beauty Therapy</Link>
+              </li>
+              <li className="bg-[#c79a96] text-white hover:bg-[#a87672]">
+                <Link to="/blog">My Blog</Link>
+              </li>
+              <li className="bg-[#c79a96] text-white hover:bg-[#a87672]">
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="hidden lg:flex">
+          {" "}
+          {/* Desktop menu */}
           <ul className="menu menu-horizontal px-1">
             <li>
               <Link to="/">About</Link>
