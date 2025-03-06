242e56b9007babd8c55aaef2f84bf674bb4fed26
Simiso K Mdluli <simisok22@gmail.com>
2025-03-06 12:32:40 +0200

    New Change on About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ function About() {
           <div className="md:w-1/4 ml-5">
             <img src={aboutImage} alt="Client" className="rounded-lg shadow-lg" />
           </div>
-          <div className="md:w-1/2 md:pl-8 text-center">
+          <div className="md:w-1/2 text-center">
             <p className='text-xl'>Nonhle is an entrepreneur based in Manzini, Eswatini. She is the Owner and CEO of Manzini Bright Light, a business whose focus is yet to be determined. She has a background in beauty and education, having studied at LH LAbeauty and African Legacy University.<br></br>Nonhle completed her high school education at Manzini Nazarene High School. She has built a presence in her community, being followed by a growing number of people on social media.<br></br>With her roots and current residence in Manzini, Nonhle is deeply connected to her hometown. Her educational and entrepreneurial background suggests she is passionate about personal development, business, and possibly the beauty industry.</p>
           </div>
         </div>
